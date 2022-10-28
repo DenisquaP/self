2b98d4be5840b614f59bb9ef98e376e891156d67
DenisquaP <timchamkin1@yandex.ru>
2022-10-28 13:07:23 +0300

    foe

--- lab7/header.h
+++ lab7/header.h
@@ -1,3 +1,19 @@
 #include <iostream>
-#include "string"
+#include <string>
 using namespace std;
+
+
+void print(string s)
+{
+
+    cout << s << "\n";
+
+};
+
+
+bool in(string s, string symbol){
+    for (int i = 0; i < s.size(); i++){
+        bool flag = false;
+        if(symbol == s[i]){ flag = true; return flag;}
+    }
+};
--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -1,1 +1,21 @@
 #include "header.h"
+
+
+int main() 
+{
+    print("-----------------------------------first task--------------------------");
+    string s1;
+    print("Enter your string");
+    cin >> s1;
+    print(s1.substr(2, 4));
+
+    print("-------------------------------second task-------------------------");
+    if (in(s1, "a")){
+    print(to_string(s1.find("o")));
+}
+    else{
+        print("Not in string")
+    }
+
+    system("Pause");
+}
