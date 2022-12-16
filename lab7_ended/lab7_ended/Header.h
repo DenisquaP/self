17c0cad864be20dd4e6e11ce7a9a1e7374b45bd4
DenisquaP <timchamkin1@yandex.ru>
2022-12-16 15:41:57 +0300

    edded 1st task

---
+++ lab7_ended/lab7_ended/Header.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <sstream>
+#define _USE_MATH_DEFINES
+#define PI 3.141592653589793
+
+using namespace std;
+
---
+++ lab7_ended/lab7_ended/lab7_ended.cpp
@@ -0,0 +1,24 @@
+﻿#include "Header.h"
+
+int main() {
+	// 1st task
+	string user_string;
+	cout << "enter string\n";
+	getline(cin, user_string);
+	cout << user_string[1] << user_string[2] << user_string[3];
+	for (int i = 0; i < user_string.size(); i++) {
+		if (user_string[i] == 'a') cout << i << endl;
+	}
+	// 2nd task
+	string name; 
+	cout << "enter your name\n";
+
+
+	// 3rd task
+	double radius;
+	cout << "enter radius\n";
+	cin >> radius;
+	cout << ((2 * PI * radius) * 100)/100;
+	
+
+}
